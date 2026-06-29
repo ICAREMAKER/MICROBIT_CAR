d1822b45e17b86730556f3dbce6bd9cbea3c6329
ICAREMAKER <107696317+ICAREMAKER@users.noreply.github.com>
2026-06-29 14:43:49 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+input.onLogoEvent(TouchButtonEvent.Released, function () {
+    if (Verrou == 1) {
+        Verrou = 0
+    }
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    if (Verrou == 0) {
+        Mode = 1
+        Verrou = 1
+    }
+})
+let Verrou = 0
+let Mode = 0
+let Correctif_Servo1 = -10
+Mode = 0
 basic.showLeds(`
     # # . # #
     # # . # #
@@ -13,22 +28,38 @@ basic.showLeds(`
     . # # # .
     `)
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        servos.P0.setAngle(30)
-        images.arrowImage(ArrowNames.South).showImage(0)
-    }
-    if (input.buttonIsPressed(Button.B)) {
-        servos.P0.setAngle(90)
-        images.arrowImage(ArrowNames.North).showImage(0)
-    }
-    if (!(input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
-        servos.P0.setAngle(45)
+    if (Mode == 0) {
         basic.showLeds(`
-            # # . # #
-            # # . # #
-            . . . . .
-            # . . . #
-            . # # # .
+            . . # # .
+            . # . # .
+            . # . # .
+            . # . # .
+            . # # . .
             `)
+        if (input.buttonIsPressed(Button.A)) {
+            servos.P0.setAngle(0)
+            images.arrowImage(ArrowNames.South).showImage(0)
+        }
+        if (input.buttonIsPressed(Button.B)) {
+            servos.P0.setAngle(75)
+            images.arrowImage(ArrowNames.North).showImage(0)
+        }
+        if (!(input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
+            servos.P0.setAngle(35)
+        }
+        if (Math.abs(Mode) == 1) {
+            basic.showLeds(`
+                . . # . .
+                . # # . .
+                . . # . .
+                . . # . .
+                . # # # .
+                `)
+            servos.P1.run(10)
+            servos.P2.run(Correctif_Servo1 + -10)
+            basic.pause(2000)
+            servos.P1.stop()
+            servos.P2.stop()
+        }
     }
 })
