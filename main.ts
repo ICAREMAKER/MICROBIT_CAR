d6b4a8d5d78a26d3f73ded43ef08fef3794a20e1
ICAREMAKER <107696317+ICAREMAKER@users.noreply.github.com>
2026-06-29 11:41:21 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -14,12 +14,15 @@ basic.showLeds(`
     `)
 basic.forever(function () {
     if (input.buttonIsPressed(Button.A)) {
-        images.arrowImage(ArrowNames.West).showImage(0)
+        servos.P0.setAngle(30)
+        images.arrowImage(ArrowNames.South).showImage(0)
     }
     if (input.buttonIsPressed(Button.B)) {
-        images.arrowImage(ArrowNames.East).showImage(0)
+        servos.P0.setAngle(90)
+        images.arrowImage(ArrowNames.North).showImage(0)
     }
     if (!(input.buttonIsPressed(Button.A) || input.buttonIsPressed(Button.B))) {
+        servos.P0.setAngle(45)
         basic.showLeds(`
             # # . # #
             # # . # #
